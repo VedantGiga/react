e26f68b50dc42d82fade430be7e9e832865e3c15
VedantGiga <vedantvyas79@gmail.com>
2025-03-03 08:00:54 +0530

    theme switcher

---
+++ 08miniContext/src/components/Profile.jsx
@@ -0,0 +1,14 @@
+import React, {useState, useContext}from "react";
+import userContext from "../context/UserContext";
+
+
+
+function Profile() {
+    const {user} = useContext(userContext)
+
+    if(!user) return <div>Please login</div>
+
+    return <div>Welcome {user.username}</div>
+}
+
+export default Profile
